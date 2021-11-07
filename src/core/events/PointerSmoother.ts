65df2cea2c43e8063558afaef81531c49d66c32a
yuneco <yukiyuneco@gmail.com>
2021-11-07 21:19:39 +0900

    手振れ補正のロジック誤りを修正

--- src/core/events/PointerSmoother.ts
+++ src/core/events/PointerSmoother.ts
@@ -1,39 +1,65 @@
 import { PenInput } from '../canvas/PenInput'
+import { normalizeAngle } from '../coords/CoordUtil'
 import { Point } from '../coords/Point'
 import { LimittedStack } from '../misc/LimittedStack'
 
-const STRENGTH = 0.2
+const STRENGTH = 0.1
 const MIN_HIST_COUNT = 3
 
+const getVecAngle = (p0: Point, p1: Point): number => {
+  return p1.sub(p0).angle
+}
+
+const setVecAngle = (angle: number, p0: Point, p1: Point): Point => {
+  const p = p1.sub(p0)
+  return p.rotate(angle - p.angle).move(p0)
+}
+
+const mixAngle = (a0: number, a1: number, amount=0.5): number => {
+  const p0 = new Point(1, 0).rotate(a0).scale(1 - amount)
+  const p1 = new Point(1, 0).rotate(a1).scale(amount)
+  return p0.move(p1).angle
+}
+
 export class PointerSmoother {
-  private readonly inps = new LimittedStack<PenInput>(10)
+  private readonly inps = new LimittedStack<PenInput>(30)
 
   clear() {
     this.inps.clear()
   }
 
   add(inp: PenInput): PenInput {
-    if (this.inps.length < MIN_HIST_COUNT) {
+    const lastInp = this.inps.peek()
+    if (!lastInp || this.inps.length < MIN_HIST_COUNT) {
       this.inps.push(inp)
       return inp
     }
-    const angle = (this.avrAngle() - inp.point.angle) * STRENGTH
-    const point = inp.point.rotate(angle)
+    const avrAngle = this.avrAngle()
+    const newAngle = getVecAngle(lastInp.point, inp.point)
+    const angle = mixAngle(avrAngle, newAngle, 1 - STRENGTH)
+    const point = setVecAngle(angle, lastInp.point, inp.point)
+
     const pressure =
-      inp.pressure * (1 - STRENGTH) + this.avrPressure() * STRENGTH
-    const adjustedInp = { point, pressure }
+       this.avrPressure() * STRENGTH + inp.pressure * (1 - STRENGTH)
+
+      const adjustedInp = { point, pressure }
     this.inps.push(adjustedInp)
     return adjustedInp
   }
 
   private avrAngle() {
     const inps = this.inps.getItems()
-    return inps
-      .map((inp) => {
-        return inp.point.scale(1 / inp.point.length)
-      })
-      .reduce((p, c) => p.move(c), new Point())
-      .scale(1 / inps.length).angle
+    const angles = inps.map((p0, index) => {
+      const p1 = inps[index + 1]
+      if (!p1) return NaN
+      return getVecAngle(p0.point, p1.point)
+    }).filter(a => !isNaN(a))
+    const diffs = angles.map((a1, index) => {
+      const a0 = angles[index - 1]
+      if (a0 === undefined) return 0
+      return normalizeAngle(a1 - a0)
+    })
+    return normalizeAngle(angles[0] + diffs.reduce((p, c) => p + c, 0) / diffs.length)
   }
 
   private avrPressure() {
