df81b6551a5f450f6fa15d786b8e26ffffc6b947
yuneco <yukiyuneco@gmail.com>
2021-12-22 00:43:21 +0900

    リプレイ時の最後の点が欠落する問題の対応

--- src/core/canvas/strokeFuncs/splitStroke.ts
+++ src/core/canvas/strokeFuncs/splitStroke.ts
@@ -5,16 +5,40 @@ import { PenInput } from '../PenInput'
  * @param stroke ペン座標の配列
  */
 export const splitStoke = (stroke: PenInput[]): PenInput[][] => {
-  const splitPoint = stroke.findIndex(
-    (inp, index) => index > 0 && inp.pressure === 0
-  )
-  if (splitPoint === -1) return [stroke]
-  const segment = stroke.slice(0, splitPoint)
-  const rest = stroke.slice(splitPoint)
-  const isValidSegment = !(segment.length === 1 && segment[0].pressure === 0)
-  const isValidRest = !(rest.length === 1 && rest[0].pressure === 0)
-  return [
-    ...(isValidSegment ? [segment] : []),
-    ...(isValidRest ? splitStoke(rest) : []),
-  ]
+if (stroke.length === 0) return []
+
+  const result: PenInput[][] = []
+  let current: PenInput[] = [stroke[0]]
+  let isInSegment = stroke[0].pressure !== 0
+
+  for(let index = 1; index < stroke.length; index++) {
+    const inp = stroke[index]
+    
+    // 筆圧0が出現したらセグメント終了
+    if (isInSegment && inp.pressure === 0) {
+      current.push(inp)
+      result.push(current)
+      // 新しいセグメントを作成。この時点では開始と見做さない
+      current = [inp] // 筆圧0の入力はこのセグメントの終了と次のセグメントの開始の両方に含む
+      isInSegment = false
+      continue
+    }
+    // 筆圧0が続く間はセグメントの開始点を上書きし続ける
+    if (!isInSegment && inp.pressure === 0) {
+      current = [inp]
+      isInSegment = false
+      continue
+    }
+    // セグメント中は入力点をそのまま追加
+    current.push(inp)
+    isInSegment = true
+
+  }
+
+  // 最後のセグメントが途中なら追加
+  if (isInSegment) {
+    result.push(current)
+  }
+
+  return result
 }
